708188fe4e2b86e3afe00c905c34b48d37181891
michaelkirillov <kirilov.mikhail91@yandex.ru>
2022-03-22 20:17:08 +0300

    1st

---
+++ src/main/java/com/example/currencycheckerbot/CurrencyCheckerBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencycheckerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyCheckerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyCheckerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/currencycheckerbot/CurrencyCheckerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencycheckerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyCheckerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
